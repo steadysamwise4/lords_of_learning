95631aef39c73a5346c1384b455004b0e875accc
Sam Davenport <samueldavenport@att.net>
2022-03-07 15:23:49 -0600

    set up a function to form an array multiplication facts. It now only displays to the console. I plan to use this to add this array to the users account in the database, then manipulate properties of the fact data based on the user's mastery of each fact.

---
+++ client/mathFacts.js
@@ -0,0 +1,20 @@
+const multProblemsArr = [];
+const formArr1 = [0,1,2,3,4,5,6,7,8,9];
+const formArr2 = [1,2,3,4,5,6,7,8,9];
+
+const fillMultProblemsArr = function (formArr1, formArr2) {
+
+    for (let i=0;i<formArr1.length; i++) {
+        for (let j=0;j<formArr2.length;j++) {
+            const fact = formArr1[i] + " X " + formArr2[j] + " =";
+            const solution = formArr1[i] * formArr2[j];
+            const problemObj = {fact: fact, solution: solution}
+            multProblemsArr.push(problemObj)
+        }
+    }
+    console.log(multProblemsArr);
+
+};
+
+
+fillMultProblemsArr(formArr1, formArr2);
